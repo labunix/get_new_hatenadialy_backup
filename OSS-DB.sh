31dea659d66b9c65d0f9630afe71f1724c0f5be6
labunix <labunix@gmail.com>
2012-06-05 23:48:42 +0900

    OSS-DB RSS like Report mail

---
+++ OSS-DB.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# for OSS-DB
+
+# 実行するユーザに注意
+set -e
+
+if [ `id -u` -lt 500 ] ;then
+  echo "Sorry! Not Permit User"
+  exit 1
+fi
+PROXY="/etc/profile.d/proxy.sh"
+test -x ${PROXY} && . ${PROXY}
+MYPWD=~/myscripts/OSS-DB
+test -d ${MYPWD} || mkdir -p ${MYPWD}
+if [ ! -d "${MYPWD}" ];then
+  echo "Error, Not FOUND ${MYPWD}"
+  exit 1
+fi
+cd ${MYPWD}
+
+GETURL="http://www.oss-db.jp"
+GETFILE="OSS-DB.get"
+OLDFILE="OSS-DB.old"
+MYREPORT="LPIC_report_mail.txt"
+
+touch ${GETFILE}
+touch ${OLDFILE}
+touch ${MYREPORT}
+
+w3m -dump ${GETURL} > ${GETFILE}
+diff --left-column ${GETFILE} ${OLDFILE} | sed s/"<"//g > ${MYREPORT}
+if [ ! -s ${MYREPORT} ] ;then
+  echo "Do nothing"
+else
+  cat ${MYREPORT} | sed s/">"//g | \
+    mail -s "New LPIC Reports" `whoami`@`hostname -f`
+  mv ${GETFILE} ${OLDFILE}
+fi
+
+unset PROXY MYPWD GETURL GETFILENAME OLDFILENAME MYREPORT
+exit 0
